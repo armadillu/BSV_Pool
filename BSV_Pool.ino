570a28e1ce21371a4c078e291dad5e67d118ef56
Oriol Ferrer Mesia <oriol@uri.cat>
2024-10-23 23:09:35 +0200

    more

--- BSV_Pool.ino
+++ BSV_Pool.ino
@@ -7,43 +7,86 @@
 #include "WifiPass.h"  //define wifi SSID & pass
 
 #define HOST_NAME "BSV_Pool"
-
-// PROTOCOL P906-transmision-protocol_R10.pdf
-// Uses RS485 MOdbus to TTL Serial
-
 #define rxPin D1
 #define txPin D2
-//#define SERIAL_OBJ Serial
-#define SERIAL_OBJ mySerial
 
+// communicate with a BSV EVO BASIC pool chlorinator
+// PROTOCOL P906-transmision-protocol_R10.pdf
+// Uses RS232 TTL Serial, need an MAX3232 chip to connect to from an ESP8266
+
+// COMMANDS ///////////////////////////////////////////////////////
+// * SEND (always 3 bytes)
+// B1: ? 		>> 0x3f 	(ascii 63)
+// B2: CMD 		:
+//		y			>> 0x79	(ascii 121) >> software version
+// B3: EOT 		>> 0x04 	(ascii 4)
+
+// * RECEIVE (always 3 bytes)
+// B1: B2 from TX
+// B2: 
+// B3: 
+///////////////////////////////////////////////////////////////////
+
+struct PoolData{
+	int currentChlorine = 0;
+	int targetChlorine = 0;
+	int currentPH = 0;
+	int targetPH = 0;
+};
+
+String ID;
 SerialWebLog mylog;
-String buffer;
-EspSoftwareSerial::UART SERIAL_OBJ;
+EspSoftwareSerial::UART mySerial;
+PoolData poolData;
+unsigned int frameCounter = INT_MAX;
 
-void sentTest(){
-	String msg = "_HELLO_";
-	int numW = SERIAL_OBJ.write(msg.c_str(), msg.length());
-	SERIAL_OBJ.flush();
-	mylog.printf("Sent %s\n", msg.c_str());
+/////////////////////////////////////////////////////////////////////
+
+void handleMetrics() {
+
+	String idString = "{id=\"" + ID + "\",mac=\"" + WiFi.macAddress().c_str() + "\",host=\"" + HOST_NAME + "\"}";
+	String message = "";
+
+	message += "# HELP chlorineFree Free Chlorine\n";
+	message += "# TYPE chlorineFree gauge\n";
+	message += "chlorineFree" + idString + String(poolData.currentChlorine) + "\n";
+
+	message += "# HELP chlorineTarget Chlorine Target\n";
+	message += "# TYPE chlorineTarget gauge\n";
+	message += "chlorineTarget" + idString + String(poolData.targetChlorine) + "\n";
+
+	message += "# HELP phCurrent Current PH\n";
+	message += "# TYPE phCurrent gauge\n";
+	message += "phCurrent" + idString + String(poolData.currentPH) + "\n";
+
+	message += "# HELP phTarget Target PH\n";
+	message += "# TYPE phTarget gauge\n";
+	message += "phTarget" + idString + String(poolData.targetPH) + "\n";
+
+	mylog.getServer()->send(200, "text/plain", message);
 }
 
 void setup() {
 
 	mylog.setup(HOST_NAME, ssid, password);
 
-	mylog.getServer()->on("/sendTest", sentTest);
-	mylog.addHtmlExtraMenuOption("SendTest", "/sendTest");
+	//setup endpoints
+	mylog.getServer()->on("/metrics", handleMetrics);
+	mylog.addHtmlExtraMenuOption("Metrics", "/metrics");
+
+	mylog.getServer()->on("/updateSensorData", updateSensorData);
+	mylog.addHtmlExtraMenuOption("Update Sensor Data", "/updateSensorData");
 
 	mylog.print("----------------------------------------------\n");
-	String ID = String(ESP.getChipId(), HEX);
+	ID = String(ESP.getChipId(), HEX);
 	mylog.printf("%s\n",HOST_NAME);
 	mylog.printf("Booting %s ......\n", ID.c_str());
 
 	mylog.print("Serial Connection...\n");
 	pinMode(rxPin, INPUT);
 	pinMode(txPin, OUTPUT);
-	SERIAL_OBJ.begin(19200, SWSERIAL_8N1, rxPin, txPin, false);
-	delay(500);
+	mySerial.begin(19200, SWSERIAL_8N1, rxPin, txPin, false);
+	delay(100);
 
 	mylog.print("Watchdog setup...\n");
 	ESP.wdtDisable();
@@ -59,12 +102,67 @@ void setup() {
 	mylog.print("ready!\n");
 }
 
+
 void sendMessage(byte b, const String & txt){
+
 	byte msg[] = { 0x3F, 0x79, 0x04 };
 	msg[1] = b;
-	TelnetStream.print("Sending " + txt + " - 0x" + String((int)b, HEX) + " '" + String((char)b) + "'\n");
-	SERIAL_OBJ.write(msg, 3);
-	SERIAL_OBJ.flush();
+	String logLine = "Sending " + txt + " - 0x" + String((int)b, HEX) + " '" + String((char)b) + "'\n" ;
+	mylog.print(logLine);
+	TelnetStream.print(logLine);
+	
+	//send data
+	mySerial.write(msg, 3);
+	mySerial.flush();
+	
+	//wait for an answer to be ready...
+	int timeout = 0;
+	bool gotResponse = true;
+	while (mySerial.available() == 0) {
+		delay(5);
+		timeout++;
+		if(timeout > 10){
+			gotResponse = false;
+			break;
+		}
+	}
+	
+	if(gotResponse){
+
+		//read answer (should be 3 bytes)
+		static byte data[64] = {0x00};
+		int c = 0;
+		while (mySerial.available()){ 
+			data[c] = mySerial.read();
+			c++;
+		}
+
+		int value = 0;
+		if(data[0] == b){ //response must start with the requested field
+			switch(b){
+				case 0x4F: //Chlorine Target				
+					value = poolData.targetChlorine = data[1] | data[2] << 8; break;
+				case 0x6F: //Chlorine Current
+					value = poolData.currentChlorine = data[1] | data[2] << 8; break;
+				case 0x70: //PH Current
+					value = poolData.currentPH = data[1] | data[2] << 8; break;
+				case 0x50: //PH Target
+					value = poolData.targetPH = data[1] | data[2] << 8; break;
+			}
+			mylog.printf("Got a response for msg 0x%02x - %d) \n", b, value);
+		}else{
+			mylog.printf("Error, unexpected respose from BSV! (for msg 0x%02x - %s) \n", b, txt.c_str());
+		}
+	}else{
+		mylog.printf("No reponse! (for msg 0x%02x - %s) \n", b, txt.c_str());
+	}
+}
+
+void updateSensorData(){
+	sendMessage(0x4F, "ORP 0-800 Target");
+	sendMessage(0x6F, "Current ORP");
+	sendMessage(0x50, "PH Target");
+	sendMessage(0x70, "PH Measurement");
 }
 
 void loop() {
@@ -73,59 +171,24 @@ void loop() {
 	ArduinoOTA.handle();
 	ESP.wdtFeed(); //feed watchdog frequently
 
+	//handle interactive telnet session
 	switch (TelnetStream.read()) {
-
-		case '1':
-			TelnetStream.print("Print Buffer:\n");
-			TelnetStream.print(buffer);
-			buffer = "";
-			TelnetStream.print("ok\n");
-			break;
-
+		case '1': sendMessage(0x48, "Time"); break;
 		case '2': sendMessage(0x79, "Soft. Version"); break;
 		case '3': sendMessage(0x4C, "Language"); break;
 		case '4': sendMessage(0x5A, "Chlorinator Size"); break;
 		case '5': sendMessage(0x4F, "ORP 0-800 Target"); break;
 		case '6': sendMessage(0x6F, "Current ORP"); break; //got '0xab 0x02' which is 683
-		case '7': sendMessage(0x50, "PH Measurement"); break; // got '0xee 0x02 ' which is 750 (signed int)
-		case '8': sendMessage(0x4E, "Salt Concentration"); break;
-		case '9': sendMessage(0x48, "Time"); break;
-
+		case '7': sendMessage(0x50, "PH Target"); break; // got '0xee 0x02 ' which is 750 (signed int)
+		case '8': sendMessage(0x70, "PH Measurement"); break; // got '0xee 0x02 ' which is 750 (signed int)
+		case '9': sendMessage(0x4E, "Salt Concentration"); break;		
 	}
-
-	rxSerial();
 	delay(7);
-}
 
-// COMMANDS ///////////////////////////////////////////////////////
-// * SEND (always 3 bytes)
-// B1: ? 		>> 0x3f 	(ascii 63)
-// B2: CMD 		:
-//		y			>> 0x79	(ascii 121) >> software version
-// B3: EOT 		>> 0x04 	(ascii 4)
-
-// * RECEIVE (always 3 bytes)
-// B1: B2 from TX
-// B2: 
-// B3: 
-///////////////////////////////////////////////////////////////////
-
-void rxSerial() {
-	if (SERIAL_OBJ.available()) {
-		String localBuf = "";
-		static byte data[256] = {};
-		int c = 0;
-		while (SERIAL_OBJ.available()) {
-			data[c] = SERIAL_OBJ.read();
-			c++;
-		}
-		for( int i = 0; i < c; i++){
-			char aux[4];
-			sprintf(aux, "%02x", (char)data[i]);
-			localBuf += "0x" + String(aux) + " ";
-		}
-		buffer += " << '" + localBuf + "'\n'";
-		mylog.printf("Got Serial Data (%d) '%s'\n", c, localBuf.c_str());
-		TelnetStream.printf("Got Serial Data (%d) '%s'\n", c, localBuf.c_str());
+	//update sensor data
+	frameCounter++;
+	if(frameCounter > 15000 || frameCounter < 0){
+		frameCounter = 0;
+		updateSensorData();
 	}
 }
